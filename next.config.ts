aa64b5c18326c994803c41ff69fb769362f621e3
eduardbalan325 <eduardbalan325@gmail.com>
2026-09-01 13:02:52 -0700

    fix:github pages asset paths

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
